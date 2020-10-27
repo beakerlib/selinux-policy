cf8f080e726519c9e6ba95bb875eaaa08775e939
Dalibor Pospisil <dapospis@redhat.com>
2020-10-27 20:00:14 +0100

    initial version of the library wrapper

---
+++ common/lib.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# vim: dict+=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   selinux-policy library wrapper
+#   Description: This library downloads the real library(selinux-policy) from https://src.fedoraproject.org/tests/selinux.git/selinux-policy/Library/common
+#   Author: Dalibor Pospisil <dapospis@redhat.com>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Copyright (c) 2020 Red Hat, Inc.
+#
+#   This program is free software: you can redistribute it and/or
+#   modify it under the terms of the GNU General Public License as
+#   published by the Free Software Foundation, either version 2 of
+#   the License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE.  See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program. If not, see http://www.gnu.org/licenses/.
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#   library-prefix = rlSE
+#   library-version = 1
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+true <<'=cut'
+=pod
+
+=head1 NAME
+
+Library Wrapper
+
+=head1 DESCRIPTION
+
+This is a wrapper which will download the real library.
+
+=cut
+
+
+rlSELibraryLoaded() {
+  rlRun "rlImport libraries/wrapper" || rlDie 'cannot import library(libraries/wrapper)'
+  LibrariesWrapperImport "https://src.fedoraproject.org/tests/selinux.git" "" "selinux-policy/Library/common"
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#   Authors
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+true <<'=cut'
+=pod
+
+=head1 AUTHORS
+
+=over
+
+=item *
+
+Dalibor Pospisil <dapospis@redhat.com>
+
+=back
+
+=cut
